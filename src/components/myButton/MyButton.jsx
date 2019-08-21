901120f21f6591795a6c6bdecfd80268e4790d99
fuyun <scfido@gmail.com>
2019-08-21 11:10:50 +0800

    完善Readme示例

--- src/components/myButton/MyButton.jsx
+++ src/components/myButton/MyButton.jsx
@@ -1,4 +1,4 @@
-import React, {PureComponent} from 'react'
+import React, { PureComponent } from 'react'
 import t from 'prop-types'
 import styles from "./style.css";
 
@@ -10,26 +10,52 @@ import styles from "./style.css";
  * - 功能3
  * 
  * @visibleName 自定义按钮 🐻  
- * 
+ * @version 1.0.2
+ * @author [谢剑](https://github.com/scfido)
+ * @author 朱志清
  */
-export default class MyButton extends PureComponent{
-    
+export default class MyButton extends PureComponent {
+
+    constructor(props){
+        super(props);
+        this.state = {perfix : "Demo "};
+    }
+
+    /**
+     * 设置按钮文本的前缀。
+     *
+     * @param {string} text
+     * @public
+     */
+    setPerfix(text) {
+        this.setState({perfix: text})
+    }
+
     render() {
-        const { children, ...props } =this.props;
+        const { children, ...props } = this.props;
         return (
-            <button className={styles.redButton} {...props} >Demo {children}</button>
+            <button className={styles.redButton} {...props} disabled={props.disabled} >{this.state.perfix} {children}</button>
         );
     }
 }
 
 MyButton.propTypes = {
-    /** 按钮尺寸，分大中小三种 */
+    /** 按钮尺寸，分大中小三种
+     *  
+     * @deprecated 1.0.1-beta1
+    */
     size: t.oneOf(['small', 'normal', 'big']),
 
-    /** 按钮文字颜色 */
+    /** 按钮文字颜色
+     * 加@ignore后，属性表中不在显示 
+     * @ignore 
+    */
     color: t.oneOf(['green', 'bule', 'rad']),
 
-    /** 禁用按钮 */
+    /** 禁用按钮 
+     * 
+     * @since Version 1.0.0
+    */
     disabled: t.bool,
 }
 
--- src/components/myButton/Readme.md
+++ src/components/myButton/Readme.md
@@ -1,17 +1,38 @@
 
-## 基本用法
+### 基本用法
+
+常规方式显示一个控件。
 
 ```js
 <MyButton size="big">Push Me</MyButton>
 ```
 
- ```js { "props": { "className": "radButton" } }
-<MyButton>I’m transparent!</MyButton>
+```js inside Markdown
+
+initialState = { isOpen: false };
+<MyButton size="big">Push Me</MyButton>
+
 ```
+
+自定义容器属性。
+
+```js { "props": { "style": {"backgroundColor" : "lightgreen"} } }
+<MyButton disabled={true}>禁用的按钮</MyButton>
+```
+
+禁用代码编辑器。
+
+```jsx noeditor
+<MyButton>Push Me</MyButton>
+```
+
+显示一段静态代码。
 
 ```jsx static
 import React from 'react';
 ```
+
+显示一段静态html源码。
 
 ```html
 <MyButton size="big">Push Me</MyButton>
